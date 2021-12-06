ecc5a1a786e6db9e992f084d86edf226d51d6d02
luzy12345 <1956859499@qq.com>
2021-12-06 16:24:03 +0800

    12.6

--- js/cookie.js
+++ js/cookie.js
@@ -3,7 +3,7 @@ function setCookie(cname,cvalue,exdays){
     
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
 	var expires = "expires="+d.toGMTString();
-	document.cookie = cname+"="+cvalue+"; "+expires;
+	document.cookie = cname+"="+cvalue+"; "+expires+";"+"path=/";
 }
 function getCookie(cname){
 	var name = cname + "=";
